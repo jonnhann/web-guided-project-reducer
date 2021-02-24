4b7fbf7efdcc5bb6e42502f3854749ab8e139703
Josh Jacobson <joshjacobson14@gmail.com>
2021-02-23 19:26:57 -0700

    lecture work-in-progress

--- followAlong/src/actions/titleActions.js
+++ followAlong/src/actions/titleActions.js
@@ -4,11 +4,15 @@ export const UPDATE_TITLE = "UPDATE_TITLE";
 // action creator: a function that formats an action object 
 // () => {type: string, payload: any}
 
+// this is basically our "translation layer" between the components and the reducer.
+
 export default {
     toggleEditing: () => {
+        console.log("toggle editing action creator -- translating your request into reducer speak beep boop");
         return { type: TOGGLE_EDITING };
     },
     updateTitle: (title) => {
+        console.log("update title action creator -- translating your request into reducer speak beep boop");
         return {type: UPDATE_TITLE, payload: title}
     }
 }
